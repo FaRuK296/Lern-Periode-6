451a4759a923f9b2578f0606ea412003b3a709ef
FaRuK296 <f.hodzic.inf24@stud.bbbaden.ch>
2025-08-22 11:59:52 +0200

    Add files via upload

---
+++ Arena.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nijago_LA_6
+{
+    internal class Arena
+    {
+        public Arena() { 
+        
+            Arena arena = new Arena();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Nijago_LA_6
+{
+    public class Ninja
+    {
+        static void Main(string[] args)
+        {
+            Ninja ninja = new Ninja();       
+            
+        }
+    }
+}
