cea78bc72d2b217f27bd28aa20f749ced5fe6e85
FaRuK296 <f.hodzic.inf24@stud.bbbaden.ch>
2025-08-29 11:49:14 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
-﻿namespace Nijago_LA_6
+﻿using Nijago_LA_6;
+
+namespace Ninjago_LA_6
 {
     public class Ninja
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public Waffe Waffe { get; set; }
+        public Kraft Kraft { get; set; }
+
+        public Ninja(string name, int hp, Waffe waffe, Kraft kraft)
         {
-            Ninja ninja = new Ninja();       
-            
+            Name = name;
+            Hp = hp;
+            Waffe = waffe;
+            Kraft = kraft;
+        }
+
+        public void Attack(Boesewicht enemy)
+        {
+            int damage = Waffe.Damage + Kraft.Bonus;
+            enemy.Hp -= damage;
+            Console.WriteLine($"{Name} greift {enemy.Name} an und macht {damage} Schaden!");
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (HP: {Hp}, Waffe: {Waffe.Name}, Kraft: {Kraft.Element})";
         }
     }
 }
+
